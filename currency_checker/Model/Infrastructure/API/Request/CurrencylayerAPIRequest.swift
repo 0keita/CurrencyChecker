2bf3127fb739ad46c511b3573c1e4becbcd5c90e
Keita Yoshida <tanktaex@gmail.com>
2019-05-19 03:08:03 +0900

    Refactor not to use force unwrap

--- currency_checker/Model/Infrastructure/API/Request/CurrencylayerAPIRequest.swift
+++ currency_checker/Model/Infrastructure/API/Request/CurrencylayerAPIRequest.swift
@@ -13,6 +13,9 @@ protocol CurrencylayerAPIRequest: Request {
 
 extension CurrencylayerAPIRequest {
     var accessKey: String { return "393d7e91eea017841fc9bf9fe784e94f" } // TODO: encrypt
-    var baseURL: URL { return URL(string: "http://apilayer.net/api")! }
+    var baseURL: URL {
+        guard let url = URL(string: "http://apilayer.net/api") else { preconditionFailure() }
+        return url
+    }
     var method: HTTPMethod { return .get }
 }
